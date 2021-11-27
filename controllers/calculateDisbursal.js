20a989b21ad5ef3f763d16260b6dfc25ba81dfbc
Gaurav Kumar <gaurav@kudosfinance.in>
2021-11-27 15:04:59 +0530

    added monthly emi

--- controllers/calculateDisbursal.js
+++ controllers/calculateDisbursal.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 
 
 const calculateDisbursal = (req,res)=>{
@@ -7,22 +8,39 @@ const calculateDisbursal = (req,res)=>{
         sanction_amount,
        upfront_interest
     } = req.body;
-
-// gs    res.send({sanctionAmount});
+    var data = {};
 if(!sanction_amount){
-   let  (processing_fees/100) * sanction_amount;
    let gst =(18/100)*(processing_fees/100) *sanction_amount; 
    let total_charges = processing_fees_amt+gst+upfront_interest;
    let net_disburse_amt = sanction_amount-total_charges; 
- res.status(200).json({gst,total_charges,net_disburse_amt});
+ res.status(200).json({success:true,data:{gst,total_charges,net_disburse_amt}});
 }else{
-    let gst = (18/ 100) * processing_fees_amt;
-    let total_charges = processing_fees_amt+gst+upfront_interest;
-    let net_disburse_amt = sanction_amount-total_charges; 
- res.status(200).json({gst,total_charges,net_disburse_amt});
-}
- 
+    let gst = parseInt(((18/ 100) * processing_fees_amt)).toFixed(2);
+    let total_charges = parseInt(processing_fees_amt+gst+upfront_interest).toFixed(2)
+    let net_disburse_amt = parseInt(sanction_amount-total_charges).toFixed(2); 
+    let input= {
+        "principal":sanction_amount,
+        "period":loan_tenure,
+        "interest":upfront_interest
+       }
+       const disburseData = {net_disburse_amt:net_disburse_amt,gst:gst,total_charges:total_charges}
 
+       const monthlyemi =  function(input,disburseData){
+        return new Promise(function(resolve,reject){      
+         axios.post("https://lmsutils-api.onrender.com/api/emi/monthlyemi",input)
+            .then(function(resp){
+               const data = {...disburseData,...resp.data.data};
+               console.log(data);              
+              res.status(200).json({success:true,data:data});
+            })
+            .catch(function(err){
+                 reject();
+            });
+        });
+    }
+    monthlyemi(input,disburseData);
 }
+}
+
 
 module.exports = calculateDisbursal;
