b6e341ac1998cbe8b11bb364570999a060c3c5d2
Gaurav Kumar <gaurav@kudosfinance.in>
2021-11-29 18:14:14 +0530

    added yyyy-mm-dd format in emicalculator

--- controllers/emicalculator.js
+++ controllers/emicalculator.js
@@ -1,9 +1,10 @@
 
 const monthlyemi = (req,res)=>{
-    const {principal,period,interest,} =req.body;
-    const months = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"]
+    const {principal,period,interest,emi_day} =req.body;
+    const months = ["01", "02", "03", "04", "05", "06",
+        "7", "08", "09", "10", "11", "12"]
         let monthNumber = new Date().getMonth()-1;
+        let year = new Date().getFullYear();
     const tempArray = [];
     let interestPay = 0;
     let totalPay = 0;
@@ -19,8 +20,7 @@ const monthlyemi = (req,res)=>{
         const tempPeriod = month ? Number(period) : (Number(period)*12)
         let tempPrincipal = Number(principal)
         for(let i=0; i<tempPeriod; i++){
-            const emiPerMonth = {month : "" ,EMI : 0 ,Interest : 0 ,Principal : 0 ,
-                BalanceDue : tempPrincipal
+            const emiPerMonth = {EMI : 0 ,Interest : 0 ,Principal : 0 ,BalanceDue : tempPrincipal ,month: year,
             }
             emiPerMonth.month = months[(monthNumber++ >= 11) ? (monthNumber = 0) : monthNumber]
             emiPerMonth.EMI = Math.round(emiAmount);
@@ -28,6 +28,8 @@ const monthlyemi = (req,res)=>{
             emiPerMonth.Principal = Math.round((Math.round(emiAmount))-(emiPerMonth.Interest));
             emiPerMonth.BalanceDue = Math.round(emiPerMonth.BalanceDue - emiPerMonth.Principal);
             tempPrincipal = emiPerMonth.BalanceDue;
+            if(emiPerMonth.month === "01") year++;
+            emiPerMonth.month = year+"-"+String(emiPerMonth.month)+"-"+String(emi_day);
             tempArray.push(emiPerMonth)
             interestPay = interestPay + emiPerMonth.Interest;
             totalPay = totalPay + emiPerMonth.Interest + emiPerMonth.Principal;
