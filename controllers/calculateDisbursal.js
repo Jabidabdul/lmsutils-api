2723ceaf05ac0e1c73cd0a98f8d8b879fa89e429
Gaurav Kumar <gaurav@kudosfinance.in>
2021-11-30 11:26:05 +0530

    removed monthly emi from calcdisburs

--- controllers/calculateDisbursal.js
+++ controllers/calculateDisbursal.js
@@ -2,12 +2,31 @@ const { default: axios } = require("axios");
 
 const calculateDisbursal = (req,res)=>{
    const {
-        processing_fees_amt,
-        loan_tenure,
-        sanction_amount,
-        upfront_interest
+        borrowerinfo,loanrequest
     } = req.body;
-    
+
+    // const borrowerinfo = {
+    //     sanction_amount: 1000,
+    //     upfront_interest: 0
+    //   };
+    //   const loanrequest = {
+    //     // processing_fees_amt: null,
+    //     loan_tenure: 3,
+    //     // processing_fees_perc: 2
+    //   };
+
+    let {sanction_amount} = borrowerinfo;
+    let upfront_interest = borrowerinfo.upfront_interest || '0.0';
+    let {processing_fees_amt,loan_tenure,processing_fees_perc} = loanrequest;
+    if(!processing_fees_amt){
+        if(!processing_fees_perc) {
+            processing_fees_perc = 0;
+            processing_fees_amt = 0;
+        } else{
+            processing_fees_perc = parseFloat(processing_fees_perc,10); 
+            processing_fees_amt = (processing_fees_perc/100)*sanction_amount;
+        }
+    }
     let gst = parseInt(((18/ 100) * parseInt(processing_fees_amt))).toFixed(2);
     let total_charges = parseInt(parseInt(processing_fees_amt)+parseInt(gst)+parseInt(upfront_interest)).toFixed(2);
     let net_disburse_amt = parseInt(parseInt(sanction_amount)-parseInt(total_charges)).toFixed(2); 
@@ -18,24 +37,24 @@ const calculateDisbursal = (req,res)=>{
         "emi_day":5,
        }
        const disburseData = {net_disburse_amt:net_disburse_amt,gst:gst,total_charges:total_charges}
-       if(input.interest==0.0){
+    //    if(input.interest==0.0){
            const data = {...disburseData};
            res.status(200).json({success:true,data:data})
-       }else{
-        const monthlyemi =  function(input,disburseData){
-            return new Promise(function(resolve,reject){      
-             axios.post("https://lmsutils-api.onrender.com/api/emi/monthlyemi",input)
-                .then(function(resp){
-                   const data = {...disburseData,...resp.data.data};          
-                  res.status(200).json({success:true,data:data});
-                })
-                .catch(function(err){
-                     reject();
-                });
-            });
-        }
-        monthlyemi(input,disburseData); 
-       }
+    //    }else{
+    //     const monthlyemi =  function(input,disburseData){
+    //         return new Promise(function(resolve,reject){      
+    //          axios.post("https://lmsutils-api.onrender.com/api/emi/monthlyemi",input)
+    //             .then(function(resp){
+    //                const data = {...disburseData,...resp.data.data};          
+    //               res.status(200).json({success:true,data:data});
+    //             })
+    //             .catch(function(err){
+    //                  reject();
+    //             });
+    //         });
+    //     }
+    //     monthlyemi(input,disburseData); 
+    //    }
 }
 
 
