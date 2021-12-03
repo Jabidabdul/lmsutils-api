ebe0971d92faf3dcba41d7514424b4b25f650758
Gaurav Kumar <gaurav@kudosfinance.in>
2021-12-03 11:49:45 +0530

    renamed the emi values

--- controllers/emicalculator.js
+++ controllers/emicalculator.js
@@ -1,6 +1,6 @@
 
 const monthlyemi = (req,res)=>{
-    const {principal,period,interest,emi_day} =req.body;
+    const {principal,period,interest,emi_day="01"} =req.body;
     const months = ["01", "02", "03", "04", "05", "06",
         "7", "08", "09", "10", "11", "12"]
         let monthNumber = new Date().getMonth()-1;
@@ -9,6 +9,7 @@ const monthlyemi = (req,res)=>{
     let interestPay = 0;
     let totalPay = 0;
     let loanEmi = 0;
+    
     const findEmiAmount=(principal, interest, period)=>{
         const month = true;
         const actutalInterest = Number(interest)/12/100;
@@ -20,23 +21,23 @@ const monthlyemi = (req,res)=>{
         const tempPeriod = month ? Number(period) : (Number(period)*12)
         let tempPrincipal = Number(principal)
         for(let i=0; i<tempPeriod; i++){
-            const emiPerMonth = {EMI : 0 ,Interest : 0 ,Principal : 0 ,BalanceDue : tempPrincipal ,month: year,
+            const emiPerMonth = {total : 0 ,interest : 0 ,principal : 0 ,remaining_principle : tempPrincipal ,date: year,
             }
-            emiPerMonth.month = months[(monthNumber++ >= 11) ? (monthNumber = 0) : monthNumber]
-            emiPerMonth.EMI = Math.round(emiAmount);
-            emiPerMonth.Interest = Math.round(((emiPerMonth.BalanceDue) * (Number(interest)/100))/12);
-            emiPerMonth.Principal = Math.round((Math.round(emiAmount))-(emiPerMonth.Interest));
-            emiPerMonth.BalanceDue = Math.round(emiPerMonth.BalanceDue - emiPerMonth.Principal);
-            tempPrincipal = emiPerMonth.BalanceDue;
-            if(emiPerMonth.month === "01") year++;
-            emiPerMonth.month = year+"-"+String(emiPerMonth.month)+"-"+String(emi_day);
+            emiPerMonth.date = months[(monthNumber++ >= 11) ? (monthNumber = 0) : monthNumber]
+            emiPerMonth.total = Math.round(emiAmount);
+            emiPerMonth.interest = Math.round(((emiPerMonth.remaining_principle) * (Number(interest)/100))/12);
+            emiPerMonth.principal = Math.round((Math.round(emiAmount))-(emiPerMonth.interest));
+            emiPerMonth.remaining_principle = Math.round(emiPerMonth.remaining_principle - emiPerMonth.principal);
+            tempPrincipal = emiPerMonth.remaining_principle;
+            if(emiPerMonth.date === "01") year++;
+            emiPerMonth.date = year+"-"+String(emiPerMonth.date)+"-"+String(emi_day);
             tempArray.push(emiPerMonth)
-            interestPay = interestPay + emiPerMonth.Interest;
-            totalPay = totalPay + emiPerMonth.Interest + emiPerMonth.Principal;
+            interestPay = interestPay + emiPerMonth.interest;
+            totalPay = totalPay + emiPerMonth.interest + emiPerMonth.principal;
         }
     }
     findEmiAmount(principal,interest,period);
-    res.status(200).json({success:true,data:{loanEmi:loanEmi,totalPay:totalPay,interestPay:interestPay,monthlyData:tempArray}});
+    res.status(200).json({success:true,data:{total:loanEmi,total:totalPay,interest_pay:interestPay,emi_monthly:tempArray}});
 }
 
 module.exports = monthlyemi;  
