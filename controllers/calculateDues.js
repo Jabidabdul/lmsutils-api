3f42dd28c13e2c04c4775b1c62ca084966492cb1
Gaurav Kumar <gaurav@kudosfinance.in>
2021-12-03 16:42:53 +0530

    added dues

--- controllers/calculateDues.js
+++ controllers/calculateDues.js
@@ -1,5 +1,5 @@
 
-const monthlyemi = (req,res)=>{
+const calculateDues = (req,res)=>{
     const {principal,period,interest,emi_day="01"} =req.body;
     const months = ["01", "02", "03", "04", "05", "06",
         "7", "08", "09", "10", "11", "12"]
@@ -9,7 +9,6 @@ const monthlyemi = (req,res)=>{
     let interestPay = 0;
     let totalPay = 0;
     let loanEmi = 0;
-
 
     const findEmiAmount=(principal, interest, period)=>{
         const month = true;
@@ -22,23 +21,25 @@ const monthlyemi = (req,res)=>{
         const tempPeriod = month ? parseInt(period) : (parseInt(period)*12)
         let tempPrincipal = parseInt(principal)
         for(let i=0; i<tempPeriod; i++){
-            const emiPerMonth = {total : 0 ,interest : 0 ,principal : 0 ,remaining_principle : tempPrincipal ,date: year,
+            const emiPerMonth = {total : 0 ,int_value : 0 ,principal_amount : 0 ,remaining_pricipal : tempPrincipal ,date: year,
             }
             emiPerMonth.date = months[(monthNumber++ >= 11) ? (monthNumber = 0) : monthNumber]
             emiPerMonth.total = parseFloat((emiAmount).toFixed(2));
-            emiPerMonth.interest = parseFloat((((emiPerMonth.remaining_principle) * (parseInt(interest)/100))/12).toFixed(2));
-            emiPerMonth.principal = parseFloat(((emiAmount)-(emiPerMonth.interest)).toFixed(2));
-            emiPerMonth.remaining_principle = parseFloat((emiPerMonth.remaining_principle - emiPerMonth.principal).toFixed(2));
-            tempPrincipal = parseFloat((emiPerMonth.remaining_principle)).toFixed(2);
+            emiPerMonth.int_value = parseFloat((((emiPerMonth.remaining_pricipal) * (parseInt(interest)/100))/12).toFixed(2));
+            emiPerMonth.principal_amount = parseFloat(((emiAmount)-(emiPerMonth.int_value)).toFixed(2));
+            emiPerMonth.remaining_pricipal = parseFloat((emiPerMonth.remaining_pricipal - emiPerMonth.principal_amount).toFixed(2));
+            tempPrincipal = parseFloat((emiPerMonth.remaining_pricipal)).toFixed(2);
             if(emiPerMonth.date === "01") year++;
             emiPerMonth.date = year+"-"+String(emiPerMonth.date)+"-"+String(emi_day);
+        
+            interestPay = parseFloat(interestPay.toFixed(2)) + emiPerMonth.int_value
+            totalPay = parseFloat(totalPay + emiPerMonth.int_value + emiPerMonth.principal_amount);
+            emiPerMonth.int_value = emiPerMonth.int_value.toString()+"A"
             tempArray.push(emiPerMonth)
-            interestPay = parseFloat(interestPay.toFixed(2)) + emiPerMonth.interest;
-            totalPay = parseFloat(totalPay + emiPerMonth.interest + emiPerMonth.principal);
         }
     }
     findEmiAmount(principal,interest,period);
-    res.status(200).json({success:true,data:{principal:principal,loan_emi:loanEmi,total:parseFloat(totalPay.toFixed(3)),interest_pay:interestPay,emi_monthly:tempArray}});
+    res.status(200).json({success:true,data:{principal:principal,loan_emi:loanEmi,total:parseFloat(totalPay.toFixed(3)),interest_pay:interestPay,dues:tempArray}});
 }
 
-module.exports = monthlyemi;  
+module.exports = calculateDues;  
--- routes/monthlyRoutes.js
+++ routes/monthlyRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const emicalculator = require("../controllers/emicalculator")
+const emicalculator = require("../controllers/emicalculator");
+const calculateDues = require("../controllers/calculateDues");
 
 
 router.post("/monthlyemi",emicalculator);
+router.post("/dues",calculateDues)
 module.exports = router;
